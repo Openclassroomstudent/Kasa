4c23963eb6fa1395031b3e3e16c5da8ec1476c61
Sadia <148858244+Openclassroomstudent@users.noreply.github.com>
2024-04-30 18:02:58 +0200

    initial coomit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Logo from "./components/Logo";
+import logo from "./components/Logo";
+import Router from "./Router";
 import './styles/index.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import FicheLogement from './pages/FicheLogement';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import Header from './components/Header';
-import Logo from './components/Logo';
-import Footer from './components/Footer';
 
-function App() {
-      return (
-            <BrowserRouter>
-                  <Logo />
-                  <Header />
-                  <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/FicheLogement/:id" element={<FicheLogement />} />
-                        <Route path="/About" element={<About />} />
-                        <Route path="/NotFound" element={<NotFound />} />
-                        <Route path="*" element={<NotFound />} />
-                  </Routes>
-                  <Footer />                  
-            </BrowserRouter>
-      );
+function App(){
+     return (
+        <>
+        <Logo/>
+        <Header/>
+        <Router/>
+        <Footer/>
+        </>
+
+     )
+
+
 }
-
 export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FicheLogement from "./pages/FicheLogement";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/Fichelogement/:id"
+                 element={<FicheLogement />}
+                 />
+                 <Route path="/About" element={<About/>}/>
+                 <Route path="/NotFound" element={<NotFound />}/>
+
+        </Routes>
+    );
+
+};
+
+export default Router;
